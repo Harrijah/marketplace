7b66bf136e0f533089aeb568f2e62ee7476f2c38
harrijah <andrianarivohari@gmail.com>
2022-09-30 07:04:56 +0200

    maj filtre homepage

--- app/Views/Template-parts/homenavmenu.php
+++ app/Views/Template-parts/homenavmenu.php
@@ -1,9 +1,9 @@
 <div class="navbar navbar-expand-lg navbar-light wrapper">
     <div class="buttonWrapper">
         <div class="btn-group thema">
-            <button type="button" class="active mybutton btn btn-outline-warning mytext" value="NotreSelection" data-id="produits">Produits de la semaine</button>
-            <button type="button" class="mybutton btn btn-outline-warning mytext" value="Nouveautes" data-id="nouveautes">Les nouveautés</button>
-            <button type="button" class="mybutton btn btn-outline-warning mytext" value="Promotion" data-id="promos">En promotion</button>
+            <button type="button" class="active mybutton btn btn-outline-warning mytext" value="selection" data-id="produits">Produits de la semaine</button>
+            <button type="button" class="mybutton btn btn-outline-warning mytext" value="nouveaute" data-id="nouveautes">Les nouveautés</button>
+            <button type="button" class="mybutton btn btn-outline-warning mytext" value="promo" data-id="promos">En promotion</button>
             <select name="rayon" id="" url2="<?php echo base_url('filtre/getResultat'); ?>" value="Rayon" url3="<?php echo base_url('filtre/getSlider')?>" class="selectrayon2 form-control mybutton btn btn-outline-warning mytext">
                 <?php echo App\Controllers\Assets::productrayons(); ?>
             </select>
